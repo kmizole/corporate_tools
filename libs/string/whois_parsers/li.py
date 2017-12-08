d73e04e216c10c2f6a70e04ba5f1a14072beaf30
yapoc <yapspam@yapoc.org>
2017-12-08 15:50:01 +0100

    Amélioration du parser du TLD .li

--- libs/string/whois_parsers/li.py
+++ libs/string/whois_parsers/li.py
@@ -1,9 +1,9 @@
 parser = {
   "creation_date": [ "First registration date:\n(.+)", "" ], 
   "domain_name": [ "Domain name:\n(.+)", "" ], 
-  "expiration_date": [ None, "1970-01-01T00:00:01" ], 
-  "name_servers": [ None, "Champ_impossible_\u00e0_identifier_pr\u00e9cis\u00e9ment" ], 
+  "expiration_date": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "name_servers": [ None, "IMPOSSIBLE_DE_GARANTIR_AVEC_CERTITUDE_LE_PARSING" ],
   "registrant": [ "Holder of domain name:\n(.+)", "" ], 
   "registrar": [ "Registrar:\n(.+)", "" ], 
-  "updated_date": [ None, "1970-01-01T00:00:01" ]
+  "updated_date": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ]
 }
