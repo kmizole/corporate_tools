aeb027741577dd36999883103c9938296d3681e4
yapoc <yapspam@yapoc.org>
2017-12-08 15:42:22 +0100

    Amélioration du parser du TLD .jp

--- libs/string/whois_parsers/jp.py
+++ libs/string/whois_parsers/jp.py
@@ -1,10 +1,10 @@
 parser = {
-  "creation_date": [ "\[ Created on\ ]\s?(.+)", "" ], 
-  "domain_name": [ "\[ Domain Name\ ]\s?(.+)", "" ], 
-  "expiration_date": [ "\[ Expires on\ ]\s?(.+)", "" ], 
-  "name_servers": [ "\[ Name Server\ ]\s*(.+)", "" ], 
-  "registrant": [ "\[ Registrant\ ]\s?(.+)", "" ], 
-  "registrar": [ None, "" ], 
-  "status": [ "\[ Status\ ]\s?(.+)", "" ], 
-  "updated_date": [ "\[ Last Updated\ ]\s?(.+)", "" ]
+  "creation_date": [ "\[登録年月日\]\s+(.+)", "" ], 
+  "domain_name": [ "\[Domain Name\]\s+(.+)", "" ], 
+  "expiration_date": [ "\[有効期限\]\s+(.+)", "" ], 
+  "name_servers": [ "\[Name Server\]\s+(.+)", "" ], 
+  "registrant": [ "\[Registrant\]\s+(.+)", "" ], 
+  "registrar": [ None, "IMPOSSIBLE_DE_GARANTIR_AVEC_CERTITUDE_LE_PARSING" ], 
+  "status": [ "\[状態\]\s+(.+)", "" ], 
+  "updated_date": [ "\[最終更新\]\s+(.+) ", "" ]
 }
