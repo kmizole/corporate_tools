27e0b6d82e4627f0324a3bdafb72af393f1261cb
yapoc <yapspam@yapoc.org>
2017-12-08 17:09:00 +0100

    Amélioration du parser du TLD .yt

--- libs/string/whois_parsers/yt.py
+++ libs/string/whois_parsers/yt.py
@@ -1,10 +1,10 @@
 parser = {
-  "creation_date": [ "created: \s?(.+)", "" ], 
-  "domain_name": [ "domain: \s?(.+)", "" ], 
-  "expiration_date": [ "Expiry Date: \s?(.+)", "" ], 
-  "name_servers": [ "nserver: \s*(.+)", "" ], 
-  "registrant": [ "contact: \s*(.+)", "" ], 
-  "registrar": [ "registrar: \s*(.+)", "" ], 
-  "status": [ "status: \s?(.+)", "" ], 
-  "updated_date": [ "last-update: \s?(.+)", "" ]
+  "creation_date": [ "created:\s+(.+)", "" ], 
+  "domain_name": [ "domain:\s+(.+)", "" ], 
+  "expiration_date": [ "Expiry Date:\s+(.+)", "" ], 
+  "name_servers": [ "nserver:\s+(.+)", "" ], 
+  "registrant": [ "contact:\s+(.+)", "" ], 
+  "registrar": [ "registrar:\s+(.+)", "" ], 
+  "status": [ "status:\s+(.+)", "" ], 
+  "updated_date": [ "last-update:\s+(.+)", "" ]
 }
