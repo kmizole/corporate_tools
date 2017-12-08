2e05702324d3c3571709f53a5d3dec8088d39b31
yapoc <yapspam@yapoc.org>
2017-12-08 11:05:16 +0100

    Amélioration du parser du TLD .cz

--- libs/string/whois_parsers/cz.py
+++ libs/string/whois_parsers/cz.py
@@ -1,10 +1,10 @@
 parser = {
   "creation_date": [ "registered: \s?(.+)", "" ], 
-  "domain_name": [ "Domain: \s?(.+)", "" ], 
+  "domain_name": [ "domain:\s+(.+)", "" ], 
   "expiration_date": [ "expire: \s?(.+)", "" ], 
   "name_servers": [ "nserver: \s*(.+) ", "" ], 
   "registrant": [ "registrant: \s?(.+)", "" ], 
   "registrar": [ "registrar: \s?(.+)", "" ], 
-  "status": [ "Status: \s?(.+)", "" ], 
+  "status": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
   "updated_date": [ "changed: \s?(.+)", "" ]
 }
