b98fe240eeb54da29d3758e5897ab953ee585e46
yapoc <yapspam@yapoc.org>
2017-12-08 10:59:01 +0100

    Ajout du parser du TLD .co

--- libs/string/whois_parsers/co.py
+++ libs/string/whois_parsers/co.py
@@ -1,10 +1,10 @@
 parser = {
-  "creation_date": [ "Domain Registration Date: \s?(.+)", "" ], 
+  "creation_date": [ "Creation Date: \s?(.+)", "" ], 
   "domain_name": [ "Domain Name: \s?(.+)", "" ], 
-  "expiration_date": [ "Domain Expiration Date: \s?(.+)", "" ], 
+  "expiration_date": [ "Registry Expiry Date: \s?(.+)", "" ], 
   "name_servers": [ "Name Server: \s*(.+)\s*", "" ], 
   "registrant": [ "Registrant Organization: \s?(.+)", "" ], 
   "registrar": [ "Registrar: \s?(.+)", "" ], 
   "status": [ "Status: \s?(.+)", "" ], 
-  "updated_date": [ "Domain Last Updated Date: \s?(.+)", "" ]
+  "updated_date": [ "Updated Date: \s?(.+)", "" ]
 }
