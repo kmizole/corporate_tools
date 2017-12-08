933eb1b848c96463c3b646c1571b2f377adf771c
yapoc <yapspam@yapoc.org>
2017-12-08 16:57:25 +0100

    Amélioration du parser du TLD .sg

--- libs/string/whois_parsers/sg.py
+++ libs/string/whois_parsers/sg.py
@@ -2,8 +2,9 @@ parser = {
   "creation_date": [ "Creation Date:\s+(.+)", "" ], 
   "domain_name": [ "Domain Name:\s+(.+)", "" ], 
   "expiration_date": [ "Expiration Date:\s+(.+)", "" ], 
-  "name_servers": [ None, "Champ_impossible_\u00e0_identifier_pr\u00e9cis\u00e9ment" ], 
-  "registrant": [ "\n\s+Name:\s+(.+)", "" ], 
+  "name_servers": [ None, "IMPOSSIBLE_DE_GARANTIR_AVEC_CERTITUDE_LE_PARSING" ], 
+  "registrant": [ "Registrant:\r\n\r\n\s+Name:\s+(.+)", "" ], 
   "registrar": [ "Registrar:\s+(.+)", "" ], 
+  "status": [ "Domain Status:\s+(.+)", "" ],
   "updated_date": [ "Modified Date:\s+(.+)", "" ]
 }
