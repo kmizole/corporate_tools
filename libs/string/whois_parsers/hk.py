e5e0c305b0df49f3b1377085ed06dc52aa2a809e
yapoc <yapspam@yapoc.org>
2017-12-08 11:20:00 +0100

    Amélioration du parser du TLD .hz

--- libs/string/whois_parsers/hk.py
+++ libs/string/whois_parsers/hk.py
@@ -2,8 +2,9 @@ parser = {
   "creation_date": [ "Domain Name Commencement Date:\s+(.+)", "" ], 
   "domain_name": [ "Domain Name:\s+(.+)", "" ], 
   "expiration_date": [ "Expiry Date:\s+(.+)", "" ], 
-  "name_servers": [ None, "Champ_impossible_\u00e0_identifier_pr\u00e9cis\u00e9ment" ], 
+  "name_servers": [ None, "IMPOSSIBLE_DE_GARANTIR_AVEC_CERTITUDE_LE_PARSING" ],
   "registrant": [ "Registrant Contact Information:\n\nCompany English Name .+:\s+(.+)", "" ], 
   "registrar": [ "Registrar Name:\s+(.+)", "" ], 
+  "status": [ "Domain Status: (.+)", "" ],
   "updated_date": [ None, "1970-01-01T00:00:01" ]
 }
