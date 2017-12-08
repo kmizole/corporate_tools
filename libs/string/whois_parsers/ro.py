e7a53144af406441ed81e5de8b04fe52de7f415a
yapoc <yapspam@yapoc.org>
2017-12-08 16:45:17 +0100

    Amélioration du parser du TLD .ro

--- libs/string/whois_parsers/ro.py
+++ libs/string/whois_parsers/ro.py
@@ -1,9 +1,10 @@
 parser = {
   "creation_date": [ "Registered On:\s+(.+)", "" ], 
   "domain_name": [ "Domain Name:\s+(.+)", "" ], 
-  "expiration_date": [ None, "1970-01-01T00:00:01" ], 
+  "expiration_date": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL"], 
   "name_servers": [ "Nameserver:\s+(.+)", "" ], 
-  "registrant": [ None, "Champ_absent_du_registre" ], 
+  "registrant": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
   "registrar": [ "Registrar:\s+(.+)", "" ], 
-  "updated_date": [ None, "1970-01-01T00:00:01" ]
+  "status": [ "\s+Domain Status: (.+)", "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ],
+  "updated_date": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ]
 }
