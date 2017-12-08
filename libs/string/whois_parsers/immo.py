2dd24652f907af41a34b1a6707ec24ebb3b610ef
yapoc <yapspam@yapoc.org>
2017-12-08 15:23:58 +0100

    Amélioration du parser du TLD .immo

--- libs/string/whois_parsers/immo.py
+++ libs/string/whois_parsers/immo.py
@@ -3,7 +3,8 @@ parser = {
   "domain_name": [ "Domain Name:\s+(.+)", "" ], 
   "expiration_date": [ "Registry Expiry Date:\s+(.+)", "" ], 
   "name_servers": [ "Name Server:\s+(.+)", "" ], 
-  "registrant": [ "Regitrant Name:\s+(.+)", "" ], 
+  "registrant": [ "Registrant Name:\s+(.+)", "" ], 
   "registrar": [ "Registrar:\s+(.+)", "" ], 
-  "updated_date": [ "Updated Date:\s+(.*)$", "" ]
+  "status": [ "Domain Status: (.+) ", "" ],
+  "updated_date": [ "Updated Date:\s+(.*)", "" ]
 }
