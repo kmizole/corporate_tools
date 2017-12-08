b136ab10a63ad4c8787a47bbfb5c77dbb0cab7f4
yapoc <yapspam@yapoc.org>
2017-12-08 15:35:06 +0100

    Amélioration du parser du TLD .it

--- libs/string/whois_parsers/it.py
+++ libs/string/whois_parsers/it.py
@@ -1,10 +1,10 @@
 parser = {
-  "creation_date": [ "Created: \s?(.+)", "" ], 
-  "domain_name": [ "Domain: \s?(.+)", "" ], 
-  "expiration_date": [ "Expire Date: \s?(.+)", "" ], 
-  "name_servers": [ "Nameservers: \s?(.+)\s?(.+)\s?(.+)\s?(.+)", "" ], 
-  "registrant": [ "Registrant: \s?Name: \s?(.+)", "" ], 
-  "registrar": [ "Registrar: \s*Organization: \s*(.+)", "" ], 
-  "status": [ "Status: \s?(.+)", "" ], 
-  "updated_date": [ "Last Update: \s?(.+)", "" ]
+  "creation_date": [ "Created:\s+(.+)", "" ], 
+  "domain_name": [ "Domain:\s+(.+)", "" ], 
+  "expiration_date": [ "Expire Date:\s+(.+)", "" ], 
+  "name_servers": [ None, "IMPOSSIBLE_DE_GARANTIR_AVEC_CERTITUDE_LE_PARSING" ], 
+  "registrant": [ "Registrant\n\s+Organization:\s+(.+)", "" ], 
+  "registrar": [ "Registrar\n\s+Organization:\s+(.+)", "" ], 
+  "status": [ "Status:\s+(.+)", "" ], 
+  "updated_date": [ "Last Update:\s+(.+)", "" ]
 }
