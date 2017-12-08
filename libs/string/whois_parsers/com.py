d8afb981498fed31f3cbe048e0bb3d9c99bb992f
yapoc <yapspam@yapoc.org>
2017-12-08 11:01:47 +0100

    Ajout du parser du TLD .com

--- libs/string/whois_parsers/com.py
+++ libs/string/whois_parsers/com.py
@@ -1,9 +1,9 @@
 parser = {
   "creation_date": [ "Creation Date: \s?(.+)", "" ], 
   "domain_name": [ "Domain Name: \s?(.+)", "" ], 
-  "expiration_date": [ "Expiration Date: \s?(.+)", "" ], 
+  "expiration_date": [ "Registry Expiry Date: \s?(.+)", "" ], 
   "name_servers": [ "Name Server: \s*(.+)\s*", "" ], 
-  "registrant": [ "Registrant Name: \s*(.+)", "" ], 
+  "registrant": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
   "registrar": [ "Registrar: \s?(.+)", "" ], 
   "status": [ "Status: \s?(.+)", "" ], 
   "updated_date": [ "Updated Date: \s?(.+)", "" ]
