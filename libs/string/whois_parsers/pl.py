1c852fdf9b603e1a27bf8f12bd72bd4660190dcb
yapoc <yapspam@yapoc.org>
2017-12-08 16:35:59 +0100

    Amélioration du parser du TLD .pl

--- libs/string/whois_parsers/pl.py
+++ libs/string/whois_parsers/pl.py
@@ -1,10 +1,11 @@
+"""name_servers : Bancal mais ça peut valoir le coup d'essayer, surtout quand j'aurais fait le TODO pour sélectionner l'index dont j'ai besoin."""
 parser = {
-  "creation_date": [ "\ncreated: \s*(.+)\n", "" ], 
-  "domain_name": [ "Domain Name: \s?(.+)", "" ], 
-  "expiration_date": [ "Expiry Date: \s*(.+)", "" ], 
-  "name_servers": [ "\nnameservers: \s*(.+)\n\s*(.+)\n", "" ], 
-  "registrant": [ "Registrant: \n\s*(.+)", "" ], 
-  "registrar": [ "Registrar: \s?(.+)", "" ], 
-  "status": [ "\nStatus: \n\s*(.+)", "" ], 
-  "updated_date": [ "\nlast modified: \s*(.+)\n", "" ]
+  "creation_date": [ "created:\s+(.+)", "" ], 
+  "domain_name": [ "DOMAIN NAME:\s+(.+)", "" ], 
+  "expiration_date": [ "renewal date:\s+(.+)", "" ], 
+  "name_servers": [ "\s{12,}(.+)\. \r\n", "" ],
+  "registrant": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "registrar": [ "REGISTRAR:\r\n(.+)", "" ], 
+  "status": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "updated_date": [ "last modified:\s+(.+)", "" ]
 }
