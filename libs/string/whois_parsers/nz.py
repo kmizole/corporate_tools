8ff2c58f4f5d0eb6a5a9599ff4b24a2882132756
yapoc <yapspam@yapoc.org>
2017-12-08 16:20:16 +0100

    Amélioration du parser du TLD .nz

--- libs/string/whois_parsers/nz.py
+++ libs/string/whois_parsers/nz.py
@@ -1,10 +1,11 @@
 parser = {
-  "creation_date": [ "domain_dateregistered: \s?(.+)", "" ], 
-  "domain_name": [ "domain_name: \s?(.+)", "" ], 
-  "expiration_date": [ "domain_datebilleduntil: \s?(.+)", "" ], 
-  "name_servers": [ "ns_name_[ 0-9 ]{2}: \s?(.+)", "" ], 
-  "registrant": [ "registrant_contact_name: \s?(.+)", "" ], 
-  "registrar": [ "registrar_name: \s?(.+)", "" ], 
-  "status": [ "query_status: \s?(.+)", "" ], 
-  "updated_date": [ "domain_datelastmodified: \s?(.+)", "" ]
+  "creation_date": [ "domain_dateregistered: (.+)\+", "" ], 
+  "domain_name": [ "domain_name: (.+)", "" ], 
+  "expiration_date": [ "domain_datebilleduntil: (.+)\+", "" ], 
+  "name_servers": [ "ns_name_[ 0-9 ]{2}: (.+)", "" ], 
+  "registrant": [ "registrant_contact_name: (.+)", "" ], 
+  "registrar": [ "registrar_name: (.+)", "" ], 
+  "status": [ "query_status: (.+)", "" ], 
+  "status": [ "ns_name_.+: (.+)", "" ],
+  "updated_date": [ "domain_datelastmodified: (.+)\+", "" ]
 }
