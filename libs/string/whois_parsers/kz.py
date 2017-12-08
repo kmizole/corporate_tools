247612c77417287023615a10e5a1e0ac32697f0f
yapoc <yapspam@yapoc.org>
2017-12-08 15:48:28 +0100

    Amélioration du parser du TLD .kz

--- libs/string/whois_parsers/kz.py
+++ libs/string/whois_parsers/kz.py
@@ -1,9 +1,10 @@
 parser = {
-  "creation_date": [ "Domain created:\s+(.+)\s+.*$", "" ], 
-  "domain_name": [ "Domain Name:\s+(.+)", "" ], 
-  "expiration_date": [ None, "" ], 
-  "name_servers": [ "^.+server\.+:\s+(.+)", "" ], 
-  "registrant": [ "Name\.+:\s+(.+)", "" ], 
-  "registrar": [ "Current Registrar:\s+(.+)", "" ], 
-  "updated_date": [ "Last modified:\s+(.*)\s+.*$", "" ]
+  "creation_date": [ "Domain created:\s+(.+) ", "" ], 
+  "domain_name": [ "Domain Name\.+: (.+)", "" ], 
+  "expiration_date": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "name_servers": [ ".+ server\.+:\s+(.+)", "" ], 
+  "registrant": [ "Organization Using Domain Name\nName\.+:\s+(.+)", "" ], 
+  "registrar": [ "Current Registar: (.+)", "" ], 
+  "status": [ "Domain status : ([a-z]+)", "" ],
+  "updated_date": [ "Last modified :\s+(.*) \(", "" ]
 }
