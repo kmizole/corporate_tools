a4061064746165fbecce40ef0e3a6b1caca58b25
yapoc <yapspam@yapoc.org>
2017-12-08 16:54:56 +0100

    Amélioration du parser du TLD .se

--- libs/string/whois_parsers/se.py
+++ libs/string/whois_parsers/se.py
@@ -3,7 +3,7 @@ parser = {
   "domain_name": [ "domain:\s+(.+)", "" ], 
   "expiration_date": [ "expires:\s+(.+)", "" ], 
   "name_servers": [ "nserver:\s+(.+)", "" ], 
-  "registrant": [ None, "Champ_absent_du_registre" ], 
+  "registrant": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
   "registrar": [ "registrar:\s+(.+)", "" ], 
   "updated_date": [ "modified:\s+(.+)", "" ]
 }
