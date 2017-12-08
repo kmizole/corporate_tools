0b01e2efee6b3d28ea955f5dab2062894353d38f
yapoc <yapspam@yapoc.org>
2017-12-08 15:26:25 +0100

    Amélioration du parser du TLD .info

--- libs/string/whois_parsers/info.py
+++ libs/string/whois_parsers/info.py
@@ -1,10 +1,10 @@
 parser = {
-  "creation_date": [ "Created On: \s?(.+)", "" ], 
-  "domain_name": [ "Domain Name: \s?(.+)", "" ], 
-  "expiration_date": [ "Registry Expiry Date: \s*(.+)", "" ], 
-  "name_servers": [ "Name Server: \s*(.+)\s*", "" ], 
-  "registrant": [ "Registrant Organization: \s?(.+)", "" ], 
-  "registrar": [ "Registrar: \s?(.+)", "" ], 
-  "status": [ "Status: \s?(.+)", "" ], 
-  "updated_date": [ "Last Updated On: \s?(.+)", "" ]
+  "creation_date": [ "Creation Date: (.+)", "" ], 
+  "domain_name": [ "Domain Name: (.+)", "" ], 
+  "expiration_date": [ "Registry Expiry Date: (.+)", "" ], 
+  "name_servers": [ "Name Server: (.+)", "" ], 
+  "registrant": [ "Registrant Organization: (.+)", "" ], 
+  "registrar": [ "Registrar: (.+)", "" ], 
+  "status": [ "Status: (.+)", "" ], 
+  "updated_date": [ "Updated Date: (.+)", "" ]
 }
