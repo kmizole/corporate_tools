dabf4cc1b0a42a9f13dc0718708324b7e6d5b55e
yapoc <yapspam@yapoc.org>
2017-12-08 11:18:22 +0100

    Amélioration du parser du TLD .fi

--- libs/string/whois_parsers/fi.py
+++ libs/string/whois_parsers/fi.py
@@ -2,8 +2,9 @@ parser = {
   "creation_date": [ "created\.+:\s+(.+)", "" ], 
   "domain_name": [ "domain\.+:\s+(.+)", "" ], 
   "expiration_date": [ "expires\.+:\s+(.+)", "" ], 
-  "name_servers": [ "nserver\.+:\s+(.+)", "" ], 
+  "name_servers": [ "nserver\.+:\s+(.+) ", "" ], 
   "registrant": [ "name\.+:\s+(.+)", "" ], 
   "registrar": [ "registrar\.+:\s+(.+)", "" ], 
-  "updated_date": [ "modified\.+:\s+(.*)$", "" ]
+  "status": [ 'status\.+: (.+)', '' ],
+  "updated_date": [ "modified\.+:\s+(.*)", "" ]
 }
