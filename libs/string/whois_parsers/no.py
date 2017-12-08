4956cececcba0fb916760efe0f9d7e3746d6e955
yapoc <yapspam@yapoc.org>
2017-12-08 16:17:01 +0100

    Amélioration du parser du TLD .no

--- libs/string/whois_parsers/no.py
+++ libs/string/whois_parsers/no.py
@@ -1,9 +1,10 @@
 parser = {
   "creation_date": [ "Created:\s+(.+)", "" ], 
   "domain_name": [ "Domain Name\.+:\s+(.+)", "" ], 
-  "expiration_date": [ None, "1970-01-01T00:00:01" ], 
+  "expiration_date": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
   "name_servers": [ "Name Server Handle\.+:\s+(.+)", "" ], 
   "registrant": [ "Legal-c Handle\.+:\s+(.+)", "" ], 
   "registrar": [ "Registrar Handle\.+:\s+(.+)", "" ], 
+  "status": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ],
   "updated_date": [ "Last updated:\s+(.+)", "" ]
 }
