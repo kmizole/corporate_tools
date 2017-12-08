6015aae8db10c90141efcea3b62a829e12ecb633
yapoc <yapspam@yapoc.org>
2017-12-08 10:56:52 +0100

    Ajout du parser du TLD .ch

--- libs/string/whois_parsers/ch.py
+++ libs/string/whois_parsers/ch.py
@@ -1,9 +1,10 @@
 parser = {
   "creation_date": [ "First registration date:\n(.+)", "" ], 
   "domain_name": [ "Domain name:\n(.+)", "" ], 
-  "expiration_date": [ None, "1970-01-01T00:00:01" ], 
-  "name_servers": [ "(.+)\s+\[ [ 0-9\. ]+\ ]", "" ], 
+  "expiration_date": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "name_servers": [ "(.+)\s+\[ [ 0-9\. ]+\ ]", "IMPOSSIBLE_DE_GARANTIR_AVEC_CERTITUDE_LE_PARSING" ], 
   "registrant": [ "Holder of domain name:\n(.+)", "" ], 
   "registrar": [ "Registrar:\n(.+)", "" ], 
-  "updated_date": [ None, "1970-01-01T00:00:01" ]
+  "status": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ],
+  "updated_date": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ]
 }
