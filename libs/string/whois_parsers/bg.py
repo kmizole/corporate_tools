1fc5150507a2020ee402778fafb5307fbdf6ee7f
yapoc <yapspam@yapoc.org>
2017-12-08 10:47:10 +0100

    Ajout du parser du TLD .bg

--- libs/string/whois_parsers/bg.py
+++ libs/string/whois_parsers/bg.py
@@ -1,9 +1,10 @@
 parser = {
-  "creation_date": [ None, "1970-01-01T00:00:01" ], 
+  "creation_date": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ],
   "domain_name": [ "DOMAIN NAME:\s+(.+)", "" ], 
-  "expiration_date": [ None, "1970-01-01T00:00:01" ], 
-  "name_servers": [ None, "Champ_impossible_\u00e0_identifier_pr\u00e9cis\u00e9ment" ], 
+  "expiration_date": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "name_servers": [ None, "IMPOSSIBLE_DE_GARANTIR_AVEC_CERTITUDE_LE_PARSING" ],
   "registrant": [ "REGISTRANT:\n(.+)", "" ], 
-  "registrar": [ None, "Champ_absent_du_registre" ], 
-  "updated_date": [ None, "1970-01-01T00:00:01" ]
+  "registrar": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "status": [ 'registration status: (.+)', '' ],
+  "updated_date": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ],
 }
