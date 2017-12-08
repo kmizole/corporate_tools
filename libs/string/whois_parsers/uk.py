db4078ff180e94b70c16640cc6b421a950062ff4
yapoc <yapspam@yapoc.org>
2017-12-08 17:06:16 +0100

    Amélioration du parser du TLD .uk

--- libs/string/whois_parsers/uk.py
+++ libs/string/whois_parsers/uk.py
@@ -1,10 +1,10 @@
 parser = {
-  "creation_date": [ "Registered on: \s*(.+)", "" ], 
-  "domain_name": [ "Domain Name: \s?(.+)", "" ], 
-  "expiration_date": [ "Expiry Date: \s*(.+)", "" ], 
-  "name_servers": [ "Name Servers: \s*(.+)\s*", "" ], 
-  "registrant": [ "Registrant: \n\s*(.+)", "" ], 
-  "registrar": [ "Registrar: \s?(.+)", "" ], 
-  "status": [ "Registration status: \n\s*(.+)", "" ], 
-  "updated_date": [ "Last updated: \s*(.+)", "" ]
+  "creation_date": [ "Registered on:\s+(.+)", "" ], 
+  "domain_name": [ "Domain name:\r\n\s+(.+)", "" ], 
+  "expiration_date": [ "Expiry date: \s*(.+)", "" ], 
+  "name_servers": [ None, "IMPOSSIBLE_DE_GARANTIR_AVEC_CERTITUDE_LE_PARSING" ], 
+  "registrant": [ "Registrant:\r\n\s+(.+)", "" ], 
+  "registrar": [ "Registrar:\r\n\s+(.+)", "" ], 
+  "status": [ "Registration status:\r\n\s+(.+)", "" ], 
+  "updated_date": [ "Last updated:\s+(.+)", "" ]
 }
