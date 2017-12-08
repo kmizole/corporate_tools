f464b60b873c3c726f08fd00c7c960e820bf5eee
yapoc <yapspam@yapoc.org>
2017-12-08 16:36:55 +0100

    Amélioration du parser du TLD .pro

--- libs/string/whois_parsers/pro.py
+++ libs/string/whois_parsers/pro.py
@@ -3,7 +3,8 @@ parser = {
   "domain_name": [ "Domain Name:\s+(.+)", "" ], 
   "expiration_date": [ "Registry Expiry Date:\s+(.+)", "" ], 
   "name_servers": [ "Name Server:\s+(.+)", "" ], 
-  "registrant": [ "Registrant Name:\s+(.+)", "" ], 
-  "registrar": [ "Registrar:\s+(.+)", "" ], 
+  "registrant": [ "Registrant Organization:\s+(.+)", "" ], 
+  "registrar": [ "Registrar:\s+(.+)", "" ],
+  "status": [ "Domain Status: (.+) ", "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ],
   "updated_date": [ "Updated Date:\s+(.+)", "" ]
 }
