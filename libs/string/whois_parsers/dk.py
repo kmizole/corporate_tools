5fd6e0a71a846b248737b4739695ac656129756b
yapoc <yapspam@yapoc.org>
2017-12-08 11:11:57 +0100

    Amélioration du parser du TLD .dk

--- libs/string/whois_parsers/dk.py
+++ libs/string/whois_parsers/dk.py
@@ -3,7 +3,8 @@ parser = {
   "domain_name": [ "Domain:\s+(.+)", "" ], 
   "expiration_date": [ "Expires:\s+(.+)", "" ], 
   "name_servers": [ "Hostname:\s+(.+)", "" ], 
-  "registrant": [ None, "Champ_impossible_\u00e0_identifier_pr\u00e9cis\u00e9ment" ], 
-  "registrar": [ None, "Champ_absent_du_registre" ], 
-  "updated_date": [ None, "1970-01-01T00:00:01" ]
+  "registrant": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ],
+  "registrar": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "status": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ],
+  "updated_date": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ]
 }
