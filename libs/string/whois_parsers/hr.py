c683091e7fe21f437ed701f950a4d73b41b69a62
yapoc <yapspam@yapoc.org>
2017-12-08 11:23:48 +0100

    Amélioration du parser du TLD .hr (mmmh... c'est quand même bizarre d'avoir moins de résultats que via le client whois)

--- libs/string/whois_parsers/hr.py
+++ libs/string/whois_parsers/hr.py
@@ -1,9 +1,9 @@
 parser = {
-  "creation_date": [ None, "1970-01-01T00:00:01" ], 
-  "domain_name": [ "domain:\s+(.+)", "" ], 
-  "expiration_date": [ "expires:\s+(.+)", "" ], 
-  "name_servers": [ None, "Champ_absent_du_registre" ], 
-  "registrant": [ "descr:\s+(.+)", "" ], 
-  "registrar": [ None, "Champ_absent_du_registre" ], 
-  "updated_date": [ None, "1970-01-01T00:00:01" ]
+  "creation_date": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "domain_name": [ "domain:\s+(.+)", "IMPOSSIBLE_DE_GARANTIR_AVEC_CERTITUDE_LE_PARSING" ], 
+  "expiration_date": [ "expires:\s+(.+)", "IMPOSSIBLE_DE_GARANTIR_AVEC_CERTITUDE_LE_PARSING" ], 
+  "name_servers": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "registrant": [ "descr:\s+(.+)", "IMPOSSIBLE_DE_GARANTIR_AVEC_CERTITUDE_LE_PARSING" ], 
+  "registrar": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "updated_date": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ]
 }
