29479338ed591ec173f7e52bb2e5d46aeba30a20
yapoc <yapspam@yapoc.org>
2017-12-08 17:06:48 +0100

    Amélioration du parser du TLD .us

--- libs/string/whois_parsers/us.py
+++ libs/string/whois_parsers/us.py
@@ -1,10 +1,10 @@
 parser = {
-  "creation_date": [ "Creation Date: \s?(.+)", "" ], 
-  "domain_name": [ "Domain Name: \s?(.+)", "" ], 
+  "creation_date": [ "Creation Date: (.+)", "" ], 
+  "domain_name": [ "Domain Name: (.+)", "" ], 
   "expiration_date": [ "Registry Expiry Date: \s*(.+)", "" ], 
   "name_servers": [ "Name Server: \s*(.+)\s*", "" ], 
   "registrant": [ "Registrant Name: \s*(.+)", "" ], 
-  "registrar": [ "Registrar: \s?(.+)", "" ], 
-  "status": [ "Domain Status: \s?(.+)", "" ], 
-  "updated_date": [ "Updated Date: \s?(.+)", "" ]
+  "registrar": [ "Registrar: (.+)", "" ], 
+  "status": [ "Domain Status: (.+) ", "" ], 
+  "updated_date": [ "Updated Date: (.+)", "" ]
 }
