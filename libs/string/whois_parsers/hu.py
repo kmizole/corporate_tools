6fee287ccd4219d6a7733aa609b4760431bceea1
yapoc <yapspam@yapoc.org>
2017-12-08 14:57:05 +0100

    Amélioration du parser du TLD .hu

--- libs/string/whois_parsers/hu.py
+++ libs/string/whois_parsers/hu.py
@@ -1,9 +1,9 @@
 parser = {
   "creation_date": [ "record created:\s+(.+)", "" ], 
   "domain_name": [ "domain:\s+(.+)", "" ], 
-  "expiration_date": [ None, "1970-01-01T00:00:01" ], 
-  "name_servers": [ None, "Champ_absent_du_registre" ], 
-  "registrant": [ None, "Champ_absent_du_registre" ], 
-  "registrar": [ None, "Champ_absent_du_registre" ], 
-  "updated_date": [ None, "1970-01-01T00:00:01" ]
+  "expiration_date": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "name_servers": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "registrant": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "registrar": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "updated_date": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ]
 }
