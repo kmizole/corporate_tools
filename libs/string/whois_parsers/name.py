5f1b6b2c6a876c5deeac05d5a40eb8e8a355e8c4
yapoc <yapspam@yapoc.org>
2017-12-08 16:04:00 +0100

    Amélioration du parser du TLD .name

--- libs/string/whois_parsers/name.py
+++ libs/string/whois_parsers/name.py
@@ -1,10 +1,10 @@
 parser = {
-  "creation_date": [ "Creation Date: \s?(.+)", "" ], 
-  "domain_name": [ "Domain Name: \s?(.+)", "" ], 
-  "expiration_date": [ "Expiration Date: \s?(.+)", "" ], 
-  "name_servers": [ "Name Server: \s*(.+)\s*", "" ], 
-  "registrant": [ "Registrant Name: \s*(.+)", "" ], 
-  "registrar": [ "Registrar: \s?(.+)", "" ], 
-  "status": [ "Domain Status: \s?(.+)", "" ], 
-  "updated_date": [ "Updated Date: \s?(.+)", "" ]
+  "creation_date": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "domain_name": [ "Domain Name: (.+)", "" ], 
+  "expiration_date": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "name_servers": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "registrant": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "registrar": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ], 
+  "status": [ "Domain Status: (.+) ", "" ], 
+  "updated_date": [ None, "CHAMP_ABSENT_DU_RÉFÉRENTIEL" ]
 }
