4ef7ca69beb6426bafe39215824033d1e5f41e3a
yapoc <yapspam@yapoc.org>
2017-12-11 15:37:38 +0100

    Ajout d'un cache pour la gestion des TLDs existants.

--- 000_create_cache.py
+++ 000_create_cache.py
@@ -9,17 +9,27 @@ logging.basicConfig (
   level = logging.INFO,
 )
 from libs.string.tld import create_cache_data as tld_whois_cache_data
+from libs.string.tld import get_tlds_list
 logger = logging.getLogger (__name__)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Génération des différents fichiers de cache utilisés par les scripts.')
+  parser.add_argument('--cache-tld-list', type = argparse.FileType ('w'), \
+    help = 'Emplacement du fichier contenant la liste des TLDs.',\
+    dest = 'cache_tld_list', required = False, default = None)
   parser.add_argument('--cache-tld-whois', type = argparse.FileType ('w'), \
-    help = 'Emplacement du fichier de cache',\
-    dest = 'cache_tld_whois', required = False, default = 'cache/tld_whois.py' )
+    help = 'Emplacement du fichier contenant l\'association entre TLD & serveur de whois.',\
+    dest = 'cache_tld_whois', required = False, default = None)
   args = parser.parse_args ()
   logger.debug ("Arguments utilisés par le script : {}.".format (args))
 
+  logger.info ("Début de la génération des caches.")
   if args.cache_tld_whois:
-    logging.info ("Rafraîchissement du cache pour les associations tld <=> serveur de whois.")
+    logger.info ("Rafraîchissement du cache pour les associations tld <=> serveur de whois.")
     result = tld_whois_cache_data ()
     args.cache_tld_whois.write (json.dumps (result, sort_keys = True, indent = 2))
+
+  if args.cache_tld_list:
+    logger.info ("Rafraîchissement du cache pour les TLDs reconnus.")
+    result = get_tlds_list ()
+    args.cache_tld_list.write (json.dumps (result, sort_keys = True, indent = 2))
--- libs/string/tld.py
+++ libs/string/tld.py
@@ -7,19 +7,21 @@ import logging
 import json
 logger = logging.getLogger (__name__)
 
-TLDS_LIST_URL = "https://www.iana.org"
+IANA_TLDS_WHOIS_LIST_URL = "https://www.iana.org"
 RE_EXTRACT_TLD = re.compile ("""^.*<span class="domain tld"><a href="(?P<url>[^"]+)">.(?P<tld>[^<]+)</a>.*$""")
 RE_EXTRACT_WHOIS_SERVER = re.compile ("""^.*WHOIS Server:</b>\s(?P<server>.+)$""")
 
+TLDS_LIST_URL = "https://raw.githubusercontent.com/publicsuffix/list/master/public_suffix_list.dat"
+
 def _fetch_from_iana ():
   logger.info ("Début de la récupération des associations TLD / serveur de whois.")
-  for l in http_get ("{}/{}".format (TLDS_LIST_URL, '/domains/root/db')).split ('\n'):
+  for l in http_get ("{}/{}".format (IANA_TLDS_WHOIS_LIST_URL, '/domains/root/db')).split ('\n'):
     m = re.match (RE_EXTRACT_TLD, l)
     if m:
       yield (m.group ('tld'), _extract_from_page (m.group ('url')))
 
 def _extract_from_page (page):
-  for i in http_get ("{}/{}".format (TLDS_LIST_URL, page)).split ('\n'):
+  for i in http_get ("{}/{}".format (IANA_TLDS_WHOIS_LIST_URL, page)).split ('\n'):
     n = re.match (RE_EXTRACT_WHOIS_SERVER, i)
     if n:
       return n.group ('server')
@@ -76,3 +78,13 @@ def compute_all_domains_tld (element, use_cache = True, cache_file = 'cache/tld_
     except FileNotFoundError:
       logger.error ("Impossible d'ouvrir le fichier de cache {}; récupération des informations depuis IANA.".format (cache_file))
       yield _compute_from_iana (element)
+
+def get_tlds_list ():
+  result = []
+  for l in http_get (TLDS_LIST_URL).split ('\n'):
+    l = l.strip ()
+    if l and not l.startswith ('//'):
+      if l.startswith ('*.'):
+        l = l.replace ('*.', '')
+      result.append (utf8_to_punny (l))
+  return result
